8bfbf725dc52e2cab0e5aa87710b03f2b3bfea32
candaleadrian <candale.adrian@gmail.com>
2015-11-21 19:42:03 +0200

    FizzBuzzStart

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void TestNr3()
+        {
+            Assert.AreEqual("FizzBuzz", FizzOrBuzz(3));
+        }
+        string FizzOrBuzz(int number)
+        {
+            return "panamea";
+        }
+    }
+}
