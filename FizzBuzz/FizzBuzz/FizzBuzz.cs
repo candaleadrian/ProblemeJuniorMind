60b2696b4ff1834c71d89ab2299eb48ee146b856
candaleadrian <candale.adrian@gmail.com>
2015-11-21 19:55:05 +0200

    FizzBuzzFirstTestPassed

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -9,11 +9,18 @@ namespace FizzBuzz
         [TestMethod]
         public void TestNr3()
         {
-            Assert.AreEqual("FizzBuzz", FizzOrBuzz(3));
+            Assert.AreEqual("Fizz", FizzOrBuzz(3));
         }
         string FizzOrBuzz(int number)
         {
-            return "panamea";
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return "not multiple of 3";
+            }
         }
     }
 }
