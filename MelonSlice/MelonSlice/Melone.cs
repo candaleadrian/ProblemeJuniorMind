02011ecccec31136073e3608a4ab40976e962104
candaleadrian <candale.adrian@gmail.com>
2015-11-21 12:53:35 +0200

    TestToFailFirst

--- MelonSlice/MelonSlice/Melone.cs
+++ MelonSlice/MelonSlice/Melone.cs
@@ -4,11 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace MelonSlice
 {
     [TestClass]
-    public class UnitTest1
+    public class Melone
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Testpt1Kg()
         {
+            Assert.AreEqual ("NU", meloneSpliter(1));
+        }
+        int meloneSpliter(int weightKg)
+        {
+            return 7;
         }
     }
 }
+// x / 2
+// 1  nu
+// 2  nu
+// 3  nu
+// 4  da
+// 5  nu
+// 6  da
