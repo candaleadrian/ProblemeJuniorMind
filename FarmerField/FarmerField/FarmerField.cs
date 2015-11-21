7abc8701b6f22881397de86b5269012e64570249
candaleadrian <candale.adrian@gmail.com>
2015-11-21 14:40:59 +0200

    FieldTDD

--- FarmerField/FarmerField/FarmerField.cs
+++ FarmerField/FarmerField/FarmerField.cs
@@ -23,19 +23,10 @@ namespace FarmerField
         }
         double initialSquareFinder(int aditionalLength, int totalArea)
         {
-            double  Delta = aditionalLength * aditionalLength + 4 * totalArea;
-            double x1 = (-aditionalLength + Math.Sqrt(Delta)) / 2;
-            double x2 = (-aditionalLength - Math.Sqrt(Delta)) / 2;
-           Console.Write(x1);
-            if (x1 > 0)
-                return x1;
-            else
-                return x2;
+            double  sqrtDelta = Math.Sqrt(aditionalLength * aditionalLength + 4 * totalArea);
+            double x1 = (-aditionalLength + sqrtDelta ) / 2;
+            double x2 = (-aditionalLength - sqrtDelta) / 2;
+            return x1 > 0?x1:x2;
         }
     }
 }
-//D=b^2-4ac
-//x1=-b+RD/2a
-//b=aditionalLength
-//a=1
-//c=totalArea
