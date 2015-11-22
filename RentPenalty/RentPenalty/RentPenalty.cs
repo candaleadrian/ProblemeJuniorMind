2b0167e16c6813df7922a8336323a557280610d7
candaleadrian <candale.adrian@gmail.com>
2015-11-22 16:40:16 +0200

    RentPenaltySecondTestFor10Daysfaild

--- RentPenalty/RentPenalty/RentPenalty.cs
+++ RentPenalty/RentPenalty/RentPenalty.cs
@@ -11,10 +11,15 @@ namespace RentPenalty
         {
             Assert.AreEqual(102, calculatePenalty(100, 1));
         }
+        [TestMethod]
+        public void oneToTenDays7()
+        {
+            Assert.AreEqual(114, calculatePenalty(100, 7));
+        }
         decimal calculatePenalty(int rent, int daysDelay)
         {
             decimal penalty = rent * 2 / 100;
-            return rent + penalty ;
+            return rent + penalty;
         }
     }
 }
