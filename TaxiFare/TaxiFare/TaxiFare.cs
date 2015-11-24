c7c5aa73bc1a9733b9e30893cab6d161e649a5b3
candaleadrian <candale.adrian@gmail.com>
2015-11-24 21:18:51 +0200

    TaxiFareDayShortDistance

---
+++ TaxiFare/TaxiFare/TaxiFare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaxiFare
+{
+    [TestClass]
+    public class TaxiFare
+    {
+        [TestMethod]
+        public void TestDayFareShortDistance()
+        {
+            Assert.AreEqual(5, CalculateTaxiFare(1, 8));
+        }
+        decimal CalculateTaxiFare(int distanceInKm, int hour)
+        {
+            return distanceInKm * 5;
+        }
+    }
+}
