a11122c10dd605807da3e78ea740e9538f857b15
candaleadrian <candale.adrian@gmail.com>
2015-11-25 20:38:43 +0200

    ArToRoStart

---
+++ ArabicToRoman/ArabicToRoman/ArabicToRoman.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArabicToRoman
+{
+    [TestClass]
+    public class ArabicToRoman
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
