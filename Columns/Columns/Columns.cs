41457e3bd1ff7221c4af8f2129a6e7898b1d54f2
candaleadrian <candale.adrian@gmail.com>
2015-11-29 01:15:49 +0200

    ColumnsFirstTestFailed

---
+++ Columns/Columns/Columns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Columns
+{
+    [TestClass]
+    public class Columns
+    {
+        [TestMethod]
+        public void ColumnA()
+        {
+            Assert.AreEqual("A",generateColumnName (1));
+        }
+        string generateColumnName (int number)
+        {
+            return "";
+        }
+    }
+}
