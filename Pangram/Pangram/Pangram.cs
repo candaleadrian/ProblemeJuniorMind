4076f7d7a25cdd774529c962d9ffff0efa172d05
candaleadrian <candale.adrian@gmail.com>
2015-11-28 17:11:45 +0200

    PangramFirstTestFailed

---
+++ Pangram/Pangram/Pangram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pangram
+{
+    [TestClass]
+    public class Pangram
+    {
+        [TestMethod]
+        public void PangramTest1()
+        {
+            Assert.AreEqual("a", decideIfIsAPangram("a", "ab"));
+        }
+        string decideIfIsAPangram(string alfabet, string text)
+        {
+            return "";
+        }
+    }
+}
