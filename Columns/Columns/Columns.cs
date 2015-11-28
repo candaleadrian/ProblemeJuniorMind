a1ab0875e87254d89a1d74203e249d9836f35d05
candaleadrian <candale.adrian@gmail.com>
2015-11-29 01:32:08 +0200

    ColumnsFirstColumnPassed

--- Columns/Columns/Columns.cs
+++ Columns/Columns/Columns.cs
@@ -9,10 +9,20 @@ namespace Columns
         [TestMethod]
         public void ColumnA()
         {
-            Assert.AreEqual("A",generateColumnName (1));
+            Assert.AreEqual("a",generateColumnName (1));
         }
         string generateColumnName (int number)
         {
+            string alfabet = "abcdefghijklmnopqrstuvwxyz";
+            int y = 1;
+            string column = "";
+            while(y != 0)
+            {
+                int i = (number % 26)-1;
+                y = number / 26;
+                column = column + alfabet[i];
+                return column;
+            }
             return "";
         }
     }
