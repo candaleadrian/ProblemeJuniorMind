fbefa1d6fef8ff108592a12b782ec6d2b598c9eb
candaleadrian <candale.adrian@gmail.com>
2015-11-28 12:35:44 +0200

    SquaresCounterFinished

--- SquaresCounter/SquaresCounter/SquaresCounter.cs
+++ SquaresCounter/SquaresCounter/SquaresCounter.cs
@@ -11,9 +11,19 @@ namespace SquaresCounter
         {
             Assert.AreEqual(1, countSquaresNumber(1));
         }
+        [TestMethod]
+        public void SquareEdge2()
+        {
+            Assert.AreEqual(5, countSquaresNumber(2));
+        }
+        [TestMethod]
+        public void SquareEdge8()
+        {
+            Assert.AreEqual(204, countSquaresNumber(8));
+        }
         int countSquaresNumber(int squareEdge)
         {
-            return 0;
+            return squareEdge * (squareEdge +1)*(2*squareEdge +1)/6;
         }
     }
 }
