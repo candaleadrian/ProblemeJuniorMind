4022348702929c108681984916f503f42f9f4dd3
candaleadrian <candale.adrian@gmail.com>
2015-11-28 12:16:22 +0200

    SquaresCounterFirstTest

---
+++ SquaresCounter/SquaresCounter/SquaresCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquaresCounter
+{
+    [TestClass]
+    public class SquaresCounter
+    {
+        [TestMethod]
+        public void SquareEdge1()
+        {
+            Assert.AreEqual(1, countSquaresNumber(1));
+        }
+        int countSquaresNumber(int squareEdge)
+        {
+            return 0;
+        }
+    }
+}
