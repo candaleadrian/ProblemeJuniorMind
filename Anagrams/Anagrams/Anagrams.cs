13ea3e1afa077d41bdcd8f61da9403aaf01ca528
candaleadrian <candale.adrian@gmail.com>
2015-12-01 17:51:18 +0200

    AnagramsFirstTestFailed

---
+++ Anagrams/Anagrams/Anagrams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagrams
+{
+    [TestClass]
+    public class Anagrams
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, "ab");
+        }
+        int CalculateAnagramsNumber(string Text)
+        {
+            return 0;
+        }
+    }
+}
