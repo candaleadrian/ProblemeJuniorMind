7c4c20e12c551c3f88736e07bd0975e71a6a477a
candaleadrian <candale.adrian@gmail.com>
2015-12-01 23:00:49 +0200

    LunchFinished2

--- Lunch/Lunch/Lunch.cs
+++ Lunch/Lunch/Lunch.cs
@@ -16,19 +16,22 @@ namespace Lunch
         {
             Assert.AreEqual(12, CalculateLowestCommonMultiple(4, 6));
         }
+        [TestMethod]
+        public void Test20and30()
+        {
+            Assert.AreEqual(60, CalculateLowestCommonMultiple(20, 30));
+        }
         int CalculateLowestCommonMultiple(int number1, int number2)
         {
-            int lowestCommonMultiple = 0;
             for (int i = 1;; i++)
             {
                 if (i%number1==0 && i%number2 == 0)
                 {
-                    lowestCommonMultiple = i;
+                    return  i;
                     break;
                 }
 
             }
-            return lowestCommonMultiple;
         }
     }
 }
