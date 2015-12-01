395c315246c990b862fe3811eeaa35582db82fee
candaleadrian <candale.adrian@gmail.com>
2015-12-01 22:50:59 +0200

    LunchFirstTestFail

---
+++ Lunch/Lunch/Lunch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch
+{
+    [TestClass]
+    public class Lunch
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, CalculateLowestCommonDenominator(1, 2));
+        }
+        int CalculateLowestCommonDenominator(int number1, int number2)
+        {
+            return 0;
+        }
+    }
+}
