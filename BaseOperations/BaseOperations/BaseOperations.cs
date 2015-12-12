a57a914549a25996bd00513e78751d605fabc7ac
candaleadrian <candale.adrian@gmail.com>
2015-12-12 13:34:41 +0200

    BaseOperationStart

---
+++ BaseOperations/BaseOperations/BaseOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseOperations
+{
+    [TestClass]
+    public class BaseOperations
+    {
+        [TestMethod]
+        public void Transform1FromBase10ToBase2()
+        {
+            CollectionAssert.AreEqual(new int[] { 1 },Transform1FromBase10ToBase2(1));
+        }
+
+        int[] Transform1FromBase10ToBase2(int number)
+        {
+            int[] base2Array = { 1 };
+            return base2Array;
+        }
+    }
+}
