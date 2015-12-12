30696f7a4c96e935fb7abf87fdd559b20844eefd
candaleadrian <candale.adrian@gmail.com>
2015-12-12 13:59:59 +0200

    BaseOperationNumber1toBase2

--- BaseOperations/BaseOperations/BaseOperations.cs
+++ BaseOperations/BaseOperations/BaseOperations.cs
@@ -12,10 +12,16 @@ namespace BaseOperations
         {
             CollectionAssert.AreEqual(new int[] { 1 },Transform1FromBase10ToBase2(1));
         }
+        [TestMethod]
+        public void Transform2FromBase10ToBase2()
+        {
+            CollectionAssert.AreEqual(new int[] { 1 , 0 }, Transform1FromBase10ToBase2(1));
+        }
 
         int[] Transform1FromBase10ToBase2(int number)
-        {
-            int[] base2Array = { 1 };
+        {    
+            int[] base2Array = { 0};
+            base2Array[0] = number % 2;
             return base2Array;
         }
     }
