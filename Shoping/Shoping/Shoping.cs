ff4bf533eeab85622eb546ee51ce0f7c96085c58
candaleadrian <candale.adrian@gmail.com>
2015-12-30 22:10:14 +0200

    Shop

---
+++ Shoping/Shoping/Shoping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shoping
+{
+    [TestClass]
+    public class Shoping
+    {
+        [TestMethod]
+        public void ShouldCalculateTotalForOneProduct()
+        {
+            Assert.AreEqual(15, CalculateTotal(products.milk));
+        }        
+        enum products { milk= (int) 15.50, water = 25};    
+        decimal CalculateTotal(object product)
+        {
+            return (decimal) products.milk;
+        }
+    }
+}
