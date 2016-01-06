2293d211cdeb013a1865927ad1951419e9c969b7
candaleadrian <candale.adrian@gmail.com>
2016-01-06 10:30:22 +0200

    RandomFunctionIsWorking

---
+++ Password/Password/Password.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Password
+{
+    [TestClass]
+    public class Password
+    {
+        [TestMethod]
+        public void ShouldReturnAPaswordContainingOneRandomLeter()
+        {
+            Assert.AreEqual(0, GeneratePassword(3,1).Length);
+        }
+        [TestMethod]
+        public void ShouldReturnARandomValueSmallerThenSix()
+        {
+            Assert.IsTrue(ReturnRandomNumberFromNullToSpecified(6)<6);
+        }
+        string GeneratePassword(int passLength, byte option)
+        {
+            return "";
+        }
+        public Random random = new Random();
+        int ReturnRandomNumberFromNullToSpecified(int upperLimit)
+        {
+            return random.Next(0,upperLimit);
+        }
+        public struct PasswordOptions
+        {
+            public string name;
+            public string type;
+            public int number;
+            public int selectionNumber;
+        }
+        public PasswordOptions[] Options =
+        {
+            new PasswordOptions {name = "smallLeters", type = "abcdefghijklmnopqrstuvwxyz",number = 6, selectionNumber=1 }
+        };
+    }
+}
