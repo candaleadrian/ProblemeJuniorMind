fda3e61c73ab1826b861f50575a698d0923d5a27
candaleadrian <candale.adrian@gmail.com>
2016-01-10 17:02:03 +0200

    CantFindCorrectTest

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -18,23 +18,43 @@ namespace Password
             var second = GeneratePassword(Options);
             Assert.AreNotEqual(first, second);
         }
+        [TestMethod]
+        public void ShouldScheckUpperCaseRandomLeters()
+        {
+            var first = GeneratePassword(Options);
+            first.Contains(char.IsUpper);
+            Assert.AreNotEqual(first, second);
+        }
         string GeneratePassword(PasswordOptions[] Option)
         {
             string password = string.Empty;
-            for (int i = 0; i < Options[0].length; i++)
-            {
-                password += (char)random.Next(97, 123);
-            }
+            if (Options[0].upperCaseCharacters>0)
+                password += ReturnRandomStringKnowingLengthAndAsciRange(Options[0].upperCaseCharacters, 65, 91);
+            if (password.Length<Options[0].length)
+            password += ReturnRandomStringKnowingLengthAndAsciRange(Options[0].length-password.Length, 97,123);
             return password;
         }
+
+        private string ReturnRandomStringKnowingLengthAndAsciRange(int length, int minRange, int maxRange)
+        {
+            string password = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                password += (char)random.Next(minRange, maxRange);
+            }
+
+            return password;
+        }
+
         public Random random = new Random();
         public struct PasswordOptions
         {
             public int length;
+            public int upperCaseCharacters;
         }
         public PasswordOptions[] Options =
         {
-            new PasswordOptions {length = 6 }
+            new PasswordOptions {length = 6, upperCaseCharacters = 3 }
         };
     }
 }
