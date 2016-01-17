446b720201f667541cefd125ac55f9ef7665cc44
candaleadrian <candale.adrian@gmail.com>
2016-01-17 21:14:23 +0200

    RunningTestForOneAndZero

--- Fibonacci/Fibonacci/Fibonacci.cs
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -7,12 +7,18 @@ namespace Fibonacci
     public class Fibonacci
     {
         [TestMethod]
-        public void ShouldReturnFibonacciSumForOne()
+        public void ShouldReturnFibonacciSumForZero()
         {
             Assert.AreEqual(0,CalculateFibonacci(0));
         }
+        [TestMethod]
+        public void ShouldReturnFibonacciSumForOne()
+        {
+            Assert.AreEqual(1, CalculateFibonacci(1));
+        }
         int CalculateFibonacci(int n)
         {
+            if (n < 2) return n;
             return 0;
         }
     }
