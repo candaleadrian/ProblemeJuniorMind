e21cbd123eab801666940b8b99d1ca2eed6adc1a
candaleadrian <candale.adrian@gmail.com>
2016-01-17 22:51:52 +0200

    StringInversionPassed

---
+++ Inversion/Inversion/Inversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inversion
+{
+    [TestClass]
+    public class Inversion
+    {
+        [TestMethod]
+        public void InverseOneCharString()
+        {
+            Assert.AreEqual("a", InverseString("a"));
+        }
+        [TestMethod]
+        public void InverseTwoCharString()
+        {
+            Assert.AreEqual("ba", InverseString("ab"));
+        }
+        [TestMethod]
+        public void InverseThreeCharString()
+        {
+            Assert.AreEqual("cba", InverseString("abc"));
+        }
+        public string InverseString(string str)
+        {
+            if (str.Length <= 1) return str;
+            return InverseString(str.Substring(1))+str[0];
+        }
+    }
+}
