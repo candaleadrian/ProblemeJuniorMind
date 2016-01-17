8087d57d9d5776ccd085ab18e65fa5197665f847
candaleadrian <candale.adrian@gmail.com>
2016-01-17 21:09:33 +0200

    FibonacciFirsTestPassed

---
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class Fibonacci
+    {
+        [TestMethod]
+        public void ShouldReturnFibonacciSumForOne()
+        {
+            Assert.AreEqual(0,CalculateFibonacci(0));
+        }
+        int CalculateFibonacci(int n)
+        {
+            return 0;
+        }
+    }
+}
