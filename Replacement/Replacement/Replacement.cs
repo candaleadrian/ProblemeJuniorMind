74811c73ca70556dc057390dc688c6eba36a208f
candaleadrian <candale.adrian@gmail.com>
2016-01-18 21:53:34 +0200

    ReplacementFirsTestPassed

---
+++ Replacement/Replacement/Replacement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Replacement
+{
+    [TestClass]
+    public class Replacement
+    {
+        [TestMethod]
+        public void ShouldReplaceACharWithAnotherString()
+        {
+            Assert.AreEqual("ccb",ReplaceACharWithAString("ab","a","cc"));
+        }
+        public string ReplaceACharWithAString(string checkedString,string toBeReplaced, string desiredString)
+        {
+            return checkedString.Replace("a",desiredString);
+        }
+
+    }
+}
