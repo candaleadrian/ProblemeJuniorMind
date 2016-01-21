b67f2cb294d044dc0b1a46f92912a99b4cd27013
candaleadrian <candale.adrian@gmail.com>
2016-01-21 19:53:46 +0200

    CalculatorFirstTestPassed

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class Calculator
+    {
+        [TestMethod]
+        public void ShouldReturnSumBetweenOneAndOne()
+        {
+            Assert.AreEqual(2, Calculate("+ 1 1"));
+        }
+        public int Calculate(string operation)
+        {
+            int num1;
+            int num2;
+            string[] opArray = operation.Split(' ');
+            int.TryParse(opArray[1], out num1);
+            int.TryParse(opArray[2], out num2);
+            return num1 + num2;
+        }
+    }
+}
