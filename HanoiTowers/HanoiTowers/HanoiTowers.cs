080fad92a91533b0a135a43c6f4fbf8cdd9358fc
candaleadrian <candale.adrian@gmail.com>
2016-01-25 21:32:30 +0200

    HanoiNotWorking

---
+++ HanoiTowers/HanoiTowers/HanoiTowers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HanoiTowers
+{
+    [TestClass]
+    public class HanoiTowers
+    {
+        [TestMethod]
+        public void ShouldReturnTheMoveForOneDisc()
+        {
+            CollectionAssert.AreEqual( { new Step('A', 'C')} Solution(1));
+        }
+        public Step[] Solution(int n)
+        {
+            Step[] steps = { new Step('A', 'C') };
+            return steps;
+        }
+        public struct Step
+        {
+            public char x, y;
+            public Step (char x,char y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+    }
+}
