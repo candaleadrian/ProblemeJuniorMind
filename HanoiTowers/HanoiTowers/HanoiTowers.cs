7ee6de410f5dcdc606ba7dabd095ad5b79298ad4
candaleadrian <candale.adrian@gmail.com>
2016-01-26 23:17:08 +0200

    HanoiTowersFinished

--- HanoiTowers/HanoiTowers/HanoiTowers.cs
+++ HanoiTowers/HanoiTowers/HanoiTowers.cs
@@ -7,7 +7,7 @@ namespace HanoiTowers
     public class HanoiTowers
     {
         [TestMethod]
-        public void ShouldReturnTheMoveForOneDisc()
+        public void ShouldReturnTheMoveForOneDisk()
         {
             Assert.AreEqual("AC",Solution(1,'A','B','C'));
         }
@@ -17,18 +17,25 @@ namespace HanoiTowers
             Assert.AreEqual("AB AC BC", Solution(2, 'A', 'B', 'C'));
         }
         [TestMethod]
-        public void ShouldReturnTheMoveForThreeDisc()
+        public void ShouldReturnTheMoveForThreeDisk()
         {
             Assert.AreEqual("AC AB CB AC BA BC AC", Solution(3, 'A', 'B', 'C'));
         }
-        public string Solution(int n, char source, char destination, char aux)
+        [TestMethod]
+        public void ShouldReturnTheNumberOfMovesForTenDisks()
+        {
+            int expected = (int)Math.Pow(2, 10) - 1;
+            int result = Solution(10, 'A', 'B', 'C').Split(' ').Length;
+            Assert.AreEqual(expected, result);
+        }
+        public string Solution(int n, char source, char aux, char destination)
         {
             string result = string.Empty;
             if (n <= 1)
-                return $"{source}{aux}";
-            result += Solution(n-1, source, aux, destination);
-            result += " " + Solution(1, source, destination, aux);
-            return result += " " + Solution(n-1, destination, source, aux);
+                return $"{source}{destination}";
+            result += Solution(n-1, source, destination, aux);
+            result += " " + Solution(1, source, aux, destination);
+            return result += " " + Solution(n-1, aux, source, destination);
         }
     }
 }
