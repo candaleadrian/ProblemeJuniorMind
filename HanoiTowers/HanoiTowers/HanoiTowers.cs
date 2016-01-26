4e5fe5e6aa28a9e463c78d09aa8fc0bf7ba016a9
candaleadrian <candale.adrian@gmail.com>
2016-01-26 21:04:34 +0200

    HanoiTwoDiskPassed

--- HanoiTowers/HanoiTowers/HanoiTowers.cs
+++ HanoiTowers/HanoiTowers/HanoiTowers.cs
@@ -9,11 +9,22 @@ namespace HanoiTowers
         [TestMethod]
         public void ShouldReturnTheMoveForOneDisc()
         {
-            Assert.AreEqual( "AC", Solution(1,'A','B','C'));
+            Assert.AreEqual("AC",Solution(1,'A','B','C'));
         }
-        public string Solution(int n, char source, char aux, char target)
-        {            
-            return "AC";
+        [TestMethod]
+        public void ShouldReturnTheMoveForTwoDisc()
+        {
+            Assert.AreEqual("AB AC BC", Solution(2, 'A', 'B', 'C'));
+        }
+        public string Solution(int n, char source, char aux, char destination)
+        {
+            string result = string.Empty;
+            if (n == 1)
+                return $"{source}{destination}";
+            result += Solution(n-1, source, destination, aux);
+            result += " " + Solution(n-1, source, aux, destination);
+            result += " " + Solution(n-1,aux,source,destination);
+            return result;
         }
     }
 }
