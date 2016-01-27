07df319b7635e0db363c26de9244ba56d0adc6d1
candaleadrian <candale.adrian@gmail.com>
2016-01-27 21:05:06 +0200

    LastNumberToPassTestIsTwentyfive

--- HanoiTowers/HanoiTowers/HanoiTowers.cs
+++ HanoiTowers/HanoiTowers/HanoiTowers.cs
@@ -28,10 +28,24 @@ namespace HanoiTowers
             int result = Solution(10, 'A', 'B', 'C').Split(' ').Length;
             Assert.AreEqual(expected, result);
         }
+        [TestMethod]
+        public void ShouldReturnTheNumberOfMovesForTwentyDisks()
+        {
+            int expected = (int)Math.Pow(2, 20) - 1;
+            int result = Solution(20, 'A', 'B', 'C').Split(' ').Length;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void ShouldReturnTheNumberOfMovesForThirtyDisks()
+        {
+            int expected = (int)Math.Pow(2, 25) - 1;
+            int result = Solution(25, 'A', 'B', 'C').Split(' ').Length;
+            Assert.AreEqual(expected, result);
+        }
         public string Solution(int n, char source, char aux, char destination)
         {
             string result = string.Empty;
-            if (n <= 1)
+            if (n == 1)
                 return $"{source}{destination}";
             result += Solution(n-1, source, destination, aux);
             result += " " + Solution(1, source, aux, destination);
