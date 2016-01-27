7152474ab41afb3e6b8ad32a93d8de26422b6151
candaleadrian <candale.adrian@gmail.com>
2016-01-27 21:23:46 +0200

    PascalTriangleFirstTest

---
+++ PascalTriangle/PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PascalTriangle
+{
+    [TestClass]
+    public class PascalTriangle
+    {
+        [TestMethod]
+        public void ShouldReturnTheFirstLine()
+        {
+            CollectionAssert.AreEqual(new int[] { 1 }, Generate(0));
+        }
+
+        private int[] Generate(int n)
+        {
+            if (n == 0)
+                return new int[] { 1 };
+            return new int[] {};
+        }
+    }
+}
