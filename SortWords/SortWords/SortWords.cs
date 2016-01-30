78090b87a281565d07cc2fd27dd027cc60e1481f
candaleadrian <candale.adrian@gmail.com>
2016-01-30 12:15:56 +0200

    FirstTestPassed

---
+++ SortWords/SortWords/SortWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortWords
+{
+    [TestClass]
+    public class SortWords
+    {
+        [TestMethod]
+        public void ShouldReturnWordForOneWordText()
+        {
+           CollectionAssert.AreEqual(new string[] {"abc"}, SortText("abc"));
+        }
+        private string[] SortText(string toBeSorted)
+        {
+            string[] words = toBeSorted.Split(' ');
+            if (words.Length <= 1)
+                return words;
+            return new string[] {};
+        }
+    }
+}
