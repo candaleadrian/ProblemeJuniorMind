13764cbf42d08c69863e294126c8895ad028d426
candaleadrian <candale.adrian@gmail.com>
2016-02-01 21:06:00 +0200

    Priority

---
+++ PrioritySort/PrioritySort/PrioritySort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrioritySort
+{
+    [TestClass]
+    public class PrioritySort
+    {
+        [TestMethod]
+        public void SouldReturnAStructOrderdByPriority()
+        {
+            
+        }
+        public struct Repairs
+        {
+            public string repair, priority;
+            public Repairs(string repair, string priority)
+            {
+                this.repair = repair;
+                this.priority = priority;
+            }
+        }
+        Repairs[] repairs =
+            {
+            new Repairs ("Radio", "High"),
+            new Repairs ("Book", "Medium"),
+            new Repairs ("Tv", "Low"),
+            new Repairs ("Car", "High"),
+            };
+    }
+}
