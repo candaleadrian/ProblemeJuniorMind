c5cbd8baefe437f4cc71b5b0b4ffd8ff08f04a29
candaleadrian <candale.adrian@gmail.com>
2016-02-04 00:14:30 +0200

    OneTestFails

--- SortWords/SortWords/SortWords.cs
+++ SortWords/SortWords/SortWords.cs
@@ -33,13 +33,33 @@ namespace SortWords
             var result = ReturnPositionOrNegative(structArray, "abc");
             Assert.AreEqual(1, result);
         }
+        [TestMethod]
+        public void ShouldReturnSortedWordsForThreeWordsText()
+        {
+            WordCount[] expected = new WordCount[2];
+            expected[0] = new WordCount("abc", 2);
+            expected[1] = new WordCount("bbc", 1);
+            expected[2] = new WordCount(null, 0);
+            CollectionAssert.AreEqual(expected, SortText("bbc abc abc"));
+        }
+        [TestMethod]
+        public void ShouldReturnSortedWordsForThreeObjectsStruct()
+        {
+            WordCount[] expected = new WordCount[2];
+            expected[0] = new WordCount("abc", 2);
+            expected[1] = new WordCount("bbc", 1);
+            WordCount[] toCheck = new WordCount[2];
+            toCheck[0] = new WordCount("bbc", 1);
+            toCheck[1] = new WordCount("abc", 2);
+            CollectionAssert.AreEqual(expected, SortStruct(toCheck));
+        }
         public WordCount[] SortText(string textToSort)
         {
             string[] textArray = textToSort.Split(' ');
             var wordCount = new WordCount[textArray.Length];
+            int length = 0;
             for (int i = 0; i < textArray.Length; i++)
             {
-                int length = 0;
                 int pos = ReturnPositionOrNegative(wordCount,textArray[i]);
                 if (pos < 0)
                 {
@@ -49,6 +69,23 @@ namespace SortWords
                 }
                 else
                     wordCount[pos].number++;       
+            }
+            return SortStruct(wordCount);
+        }
+
+        private WordCount[] SortStruct(WordCount[] wordCount)
+        {
+            for (int i = 0; i < wordCount.Length; i++)
+            {
+                for (int j = 0; j < wordCount.Length-1; j++)
+                {
+                    if (wordCount[j].number<wordCount[j+1].number)
+                    {
+                    var temp = wordCount[j+1];
+                    wordCount[j + 1] = wordCount[j];
+                    wordCount[j] = temp;
+                    }
+                }
             }
             return wordCount;
         }
