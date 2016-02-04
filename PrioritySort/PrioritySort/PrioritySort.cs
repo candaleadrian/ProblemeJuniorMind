5f20a1b77846cfe79557c562dde63719ff20152a
candaleadrian <candale.adrian@gmail.com>
2016-02-04 23:02:04 +0200

    PriorityCheckPassedTests

--- PrioritySort/PrioritySort/PrioritySort.cs
+++ PrioritySort/PrioritySort/PrioritySort.cs
@@ -9,22 +9,48 @@ namespace PrioritySort
         [TestMethod]
         public void SouldReturnAStructOrderdByPriority()
         {
-            var expected = new Repairs[] { new Repairs("shoping", 1) };
-            var result =SortByPriority(new Repairs[] { new Repairs("shoping", 1) });
+            var expected = new Repairs[] { new Repairs("shoping", "High") };
+            var result =SortByPriority(new Repairs[] { new Repairs("shoping", "High") });
             CollectionAssert.AreEqual(expected, result);
         }
-        public enum Prio { High , Medium , Low };
+        [TestMethod]
+        public void SouldReturnZeroForRepairWithHighPrio()
+        {
+            var repair = new Repairs[] { new Repairs("shoping", "High") };
+            Assert.AreEqual(0,CheckPriority(repair,0));
+        }
+        [TestMethod]
+        public void SouldReturnOneForRepairWithMediumPrio()
+        {
+            var repair = new Repairs[] { new Repairs("shoping", "Medium") };
+            Assert.AreEqual(1, CheckPriority(repair, 0));
+        }
+        public string[] Prio = { "High" , "Medium" , "Low" };
         public Repairs[] SortByPriority(Repairs[] ToBeSorted)
         {
-            var x =  Prio.Medium;
-            return new Repairs[] { new Repairs("shoping", x)};
+            for (int i = 0; i < ToBeSorted.Length; i++)
+            {
+                for (int j = i; j < ToBeSorted.Length-1; j++)
+                {
+                    if (CheckPriority(ToBeSorted,j) > CheckPriority(ToBeSorted,j+1))
+                    {
+
+                    }
+                }
+            }
+            return ToBeSorted;
         }
-        
+
+        private int CheckPriority(Repairs[] toBeSorted, int j)
+        {
+            return Array.FindIndex(Prio,priority => priority.Contains (toBeSorted[j].priority));
+        }
+
         public struct Repairs
         {
             public string repair;
-            public var priority;
-            public Repairs(string repair, var priority)
+            public string priority;
+            public Repairs(string repair, string priority)
             {
                 this.repair = repair;
                 this.priority = priority;
