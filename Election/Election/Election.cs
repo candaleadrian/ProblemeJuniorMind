43291ee1be2e4f98739d67bc96c981dd25a936ad
candaleadrian <candale.adrian@gmail.com>
2016-02-07 13:07:50 +0200

    FirstTestForQuickSortPassed

---
+++ Election/Election/Election.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Election
+{
+    [TestClass]
+    public class Election
+    {
+        [TestMethod]
+        public void ShouldOrderByVotesCandidatesFromOneElectionOffice()
+        {
+            var expected = new Results[] {new Results("Ciobanu",7),new Results("Bostanul",9) };
+            var entry = new Results[] {new Results("Bostanul", 9),new Results("Ciobanu", 7) };
+            CollectionAssert.AreEqual(expected, SortCandidatesByVotes(entry));
+        }
+        public Results[] SortCandidatesByVotes(Results[] electionResults)
+        {
+            return new Results[] { };
+        }
+        public struct Results
+        {
+            public string name;
+            public int votes;
+            public Results(string name, int votes)
+            {
+                this.name = name;
+                this.votes = votes;
+            } 
+        }
+    }
+}
--- LotoAscendingSort/LotoAscendingSort/LotoAscendingSort.cs
+++ LotoAscendingSort/LotoAscendingSort/LotoAscendingSort.cs
@@ -9,12 +9,12 @@ namespace LotoAscendingSort
         [TestMethod]
         public void ShouldReturnTheSameArrayForOneNumber()
         {
-            CollectionAssert.AreEqual(new int[] { 1 }, SortAscending(new int[] { 1 }));
+            CollectionAssert.AreEqual(new int[] { 1 }, SortAscending(new int[] { 1 }, "Selection"));
         }
         [TestMethod]
         public void ShouldSortAscendingTwoElementsArray()
         {
-            CollectionAssert.AreEqual(new int[] { 1,2 }, SortAscending(new int[] { 2,1 }));
+            CollectionAssert.AreEqual(new int[] { 1,2 }, SortAscending(new int[] { 2,1 }, "Selection"));
         }
         [TestMethod]
         public void ShouldSwapTheNumbers()
@@ -27,23 +27,51 @@ namespace LotoAscendingSort
         [TestMethod]
         public void ShouldSortAscendingFiveElementsArray()
         {
-            CollectionAssert.AreEqual(new int[] { 13, 26, 32, 42, 78 }, SortAscending(new int[] { 32,13,42,26,78 }));
+            CollectionAssert.AreEqual(new int[] { 13, 26, 32, 42, 78 }, SortAscending(new int[] { 32,13,42,26,78 }, "Selection"));
         }
-        public int[] SortAscending(int[] numbers)
+        [TestMethod]
+        public void ShouldSortTwoNumbersWithQuickMethod()
+        {
+            int[] entry = { 2, 1 };
+            int[] expected = { 1, 2 };
+            CollectionAssert.AreEqual(expected,SortNumbersQuickMethod(entry));
+        }
+        public int[] SortAscending(int[] numbers,string method)
+        {
+            if (method == "Selection")
+                return SortNumbersSelectionMethod(numbers);
+            if (method == "Quick")
+                return SortNumbersQuickMethod(numbers);
+            return new int[] { };
+        }
+        private int[] SortNumbersQuickMethod(int[] numbers)
+        {
+            int k = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[0] > numbers[i])
+                    Swap(ref numbers[i],ref numbers[++k]);
+            }
+                Swap(ref numbers[0], ref numbers[k]);
+            return numbers;
+        }
+
+        private int[] SortNumbersSelectionMethod(int[] numbers)
         {
             int[] result = new int[numbers.Length];
             if (numbers.Length <= 1)
                 return numbers;
             for (int i = 0; i < numbers.Length; i++)
             {
-                for (int j = i+1; j < numbers.Length; j++)
+                for (int j = i + 1; j < numbers.Length; j++)
                 {
                     if (numbers[i] > numbers[j])
-                        Swap(ref numbers[i],ref numbers[j]);
+                        Swap(ref numbers[i], ref numbers[j]);
                 }
             }
             return numbers;
         }
+
         private void Swap(ref int bigger,ref int smaller)
         {
             int temp = bigger;
