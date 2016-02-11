1f35a312f0230b15edb2372a27e7e5da371cf9fd
candaleadrian <candale.adrian@gmail.com>
2016-02-11 20:46:44 +0200

    SortByGeneralMeanPassedForOneStudent

--- Catalog/Catalog/Catalog.cs
+++ Catalog/Catalog/Catalog.cs
@@ -29,7 +29,64 @@ namespace Catalog
             Student bubu = new Student { name = "Bubu", mattersAndNotes = bubuMatters };
             Student[] allClass = { cucu, bubu};
             CollectionAssert.AreEqual(new string[] { "Bubu", "Cucu" }, SortStudentsAlpha(allClass));
+        }
+        [TestMethod]
+        public void ShouldReturnGeneralMeanForOneStudent()
+        {
+            Maters mathCucu = new Maters("Mathematics", new int[] { 6, 7 });
+            Maters sportCucu = new Maters("Sport", new int[] { 8, 9 });
+            Maters[] cucuMatters = { mathCucu, sportCucu };
+            Student cucu = new Student { name = "Cucu", mattersAndNotes = cucuMatters };
+            Student[] allClass = { cucu };
+            GeneralMean[] expected = { new GeneralMean("Cucu", 7.5m) };
+            CollectionAssert.AreEqual(expected, SortStudentsByGeneralMean(allClass));
+        }
+        [TestMethod]
+        public void ShouldReturnMeanForOneMatterNotes()
+        {
+            Maters mathCucu = new Maters("Mathematics", new int[] { 6, 7 });
+            Assert.AreEqual(6.5m, CalculateMeanForMatter(mathCucu));
+        }
+        public GeneralMean[] SortStudentsByGeneralMean(Student[] allClass)
+        {
+            GeneralMean[] studAndGeneralMean = new GeneralMean[allClass.Length];
+            for (int i = 0; i < allClass.Length; i++)
+            {
+                studAndGeneralMean[i].name = allClass[i].name;
+                studAndGeneralMean[i].generalMean = CalculateGeneralMean(allClass[i]);
+            }
+            return studAndGeneralMean;
+        }
 
+        private decimal CalculateGeneralMean(Student student)
+        {
+            decimal sum = 0;
+            for (int i = 0; i < student.mattersAndNotes.Length; i++)
+            {
+            sum += CalculateMeanForMatter(student.mattersAndNotes[i]);
+            }
+            return sum/student.mattersAndNotes.Length;
+        }
+
+        private decimal CalculateMeanForMatter(Maters maters)
+        {
+            int sum = 0;
+            for (int i = 0; i < maters.notes.Length; i++)
+            {
+            sum += maters.notes[i];
+            }
+            return (decimal)sum / maters.notes.Length;
+        }
+
+        public struct GeneralMean
+        {
+            public string name;
+            public decimal generalMean;
+            public GeneralMean(string name,decimal generalMean)
+            {
+                this.name = name;
+                this.generalMean = generalMean;
+            }
         }
         public string[] SortStudentsAlpha(Student[] allClass)
         {
@@ -37,7 +94,6 @@ namespace Catalog
             Array.Sort(studentsNames);
             return studentsNames;
         }
-
         private static string[] GetStudentNameArray(Student[] allClass)
         {
             string[] studentsNames = new string[allClass.Length];
@@ -62,6 +118,5 @@ namespace Catalog
                 this.notes = notes;
             }
         }
-     //   Array.Sort(words, (x, y) => x.CompareTo(y));
     }
 }
