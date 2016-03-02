1f3a98aad31f7b4448111ec05e04f94a85b30121
candaleadrian <candale.adrian@gmail.com>
2016-03-02 18:05:24 +0200

    niciCum

--- CatalogWithClass/CatalogWithClass/CatalogWithClass.cs
+++ CatalogWithClass/CatalogWithClass/CatalogWithClass.cs
@@ -10,19 +10,20 @@ namespace CatalogWithClass
         public void ShouldSortTwoStudentAlpha()
         {
             Student cucu = new Student("Cucu");
-            Grade[] cucuGrade = { new Grade("Math", new int[] { 10, 8 }),
-                                  new Grade("Sport", new int[] { 6, 8 })};
+            Grades[] cucuGrade = { new Grades("Math", new int[] { 10, 8 }),
+                                  new Grades("Sport", new int[] { 6, 8 })};
             Student bubu = new Student("Bubu");
-            Grade[] bubuGrade = { new Grade("Math", new int[] { 7, 9 }),
-                                  new Grade("Sport", new int[] { 5, 7 })};
+            Grades[] bubuGrade = { new Grades("Math", new int[] { 7, 9 }),
+                                  new Grades("Sport", new int[] { 5, 7 })};
             Student[] allClass = { cucu, bubu };
-            CollectionAssert.AreEqual(new string[] { "Bubu", "Cucu" }, SortStudentsByNameAlpha());
+        //    CollectionAssert.AreEqual(new string[] { "Bubu", "Cucu" }, SortStudentsByNameAlpha());
         }
+        
 
         public class Student
         {
             private string name;
-            private Grade[] grades;
+            private Grades[] grades;
             public Student(string name)
             {
                 this.name = name;
@@ -31,16 +32,44 @@ namespace CatalogWithClass
             {
                 return new string[] { };
             }
+            public decimal CalculateGeneralMean()
+            {
+                decimal sum = 0;
+                foreach (var subj in grades)
+                {
+          //          sum += CalculateAvrerage(subj.);
+                }
+                return 0;
+            }
 
         }
-        public class Grade
+        [TestMethod]
+        public void SouldReturnAverageForOneSubject()
+        {
+            Grades mathGrades = new Grades("Math", new int[] { 10, 8 });
+            Assert.AreEqual(9m,CalculateAvrerage(new Grades( new int[] { 10, 8 })));
+        }
+
+        public decimal CalculateAvrerage(int[] grades)
+        {
+            decimal sum = 0;
+            foreach (var grade in grades)
+            {
+                sum += grade;
+            }
+            return sum / grades.Length;
+        }
+        public class Grades
         {
             private string subject;
-            private int[] grade;
-            public Grade(string subject, int[] grade)
+            private int[] grades;
+            public Grades(string subject)
             {
                 this.subject = subject;
-                this.grade = grade;
+            }
+            public Grades(int[] grade)
+            {
+                this.grades = grade;
             }
         }
     }
