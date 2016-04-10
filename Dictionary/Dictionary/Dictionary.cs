532f8749643b0d7a755c204868179a2c8c662ddc
candaleadrian <candale.adrian@gmail.com>
2016-04-10 20:18:28 +0300

    Dictionary

---
+++ Dictionary/Dictionary/Dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dictionary
+{
+    public class Dictionary<T>
+    {
+        int[] hash = new int[3];
+        private class Dict
+        {
+            public int privious;
+            public string key;
+            public T value;
+        }
+        Dict[] dictList = new Dict[9]; 
+    }
+}
---
+++ Dictionary/Dictionary/DictionaryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dictionary
+{
+    public class DictionaryTests
+    {
+        [Fact]
+        public void ShouldoSomething()
+        {
+            int x = 1;
+            int y = 2;
+            
+            Assert.Equal(x, y);
+        }
+    }
+}
