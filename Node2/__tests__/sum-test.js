a600f089c878988329ffe0e7dc41669550c78302
candaleadrian <candale.adrian@gmail.com>
2016-08-02 20:14:24 +0300

    SecondTestPassed

--- Node2/__tests__/sum-test.js
+++ Node2/__tests__/sum-test.js
@@ -5,8 +5,12 @@ import React from "react";
 import {shallow} from "enzyme";
 
 describe('sum', () => {
-  it('adds 3 + 2 to equal 3', () => {
+  it('adds 3 + 2 to equal 5', () => {
     const sum = require('../sum');
     expect(sum(3, 2)).toBe(5);
+    });
+  it('adds 3 + 3 not equal 5', () => {
+    const sum = require('../sum');
+    expect(sum(3,3)).not.toBe(5);
     });
 });
