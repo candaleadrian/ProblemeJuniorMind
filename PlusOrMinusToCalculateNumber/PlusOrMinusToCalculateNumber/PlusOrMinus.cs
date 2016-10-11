90d1669d1f44d3786495a3ccef480b1db6e03957
candaleadrian <candale.adrian@gmail.com>
2016-10-11 23:57:53 +0300

    NotWorking

--- PlusOrMinusToCalculateNumber/PlusOrMinusToCalculateNumber/PlusOrMinus.cs
+++ PlusOrMinusToCalculateNumber/PlusOrMinusToCalculateNumber/PlusOrMinus.cs
@@ -11,44 +11,35 @@ namespace PlusOrMinusToCalculateNumber
         [TestMethod]
         public void ShoulReturnOne()
         {
-            Assert.AreEqual(1, GetNumber(7));
+            Assert.AreEqual("1+2", Combine(new int[] {1,2}));
         }
-        [TestMethod]
-        public void ShoulReturnPlus()
-        {
-            Assert.AreEqual(1, GetNumber(7));
-        }
+        
         int counterNr = 0;
         int counterSign = -1;
-        public int GetNumber(int number)
+        public string Combine(int[] numbers)
         {
-            counterNr += 1;
-            if (counterNr > number)
-                return -1;
-            return counterNr;
-        }
-        public string GetSign()
-        {
-            string[] op = new string[0];
-            int[] numbers = {1, 2};
-            string[] operation = {"+","-"};
-            op = Combine(numbers, operation);
-            return op[0];
-        }
-
-        public string[] Combine(int[] numbers, string[] operation)
-        {
-            string[] result = new string[0];
-            string tmp = string.Empty;
-            for (int i = 0; i < numbers.Length; i++)
+            string result = string.Empty;
+            int opNr = numbers.Length;
+            while (counterSign < 0)
             {
-                tmp += numbers[i];
-                for (int j = 0; j < operation.Length; j++)
+                for (int i = 0; i < opNr; i++)
                 {
-                    tmp += operation[j];
+                    result += numbers[i] + GetOperation();
                 }
             }
             return result;
+        }
+
+        private string GetOperation()
+        {
+            counterSign = 1;
+            string[] operation = { "+", "-" };
+            while (counterSign == 1)
+            {
+            if (counterNr == 0)
+                return "+";
+            }
+            throw new NotImplementedException();
         }
     }
 }
